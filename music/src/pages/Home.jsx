56ce06652df6ffb2ae6ee0c513dac9774f3b7497
Rupa-Rd <roopa.69.spr@gmail.com>
2024-03-17 12:29:32 +0530

    Home page is created successfully

---
+++ music/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Slogan from '../components/Slogan';
+
+function Home() {
+  return (
+    <div>
+        <Navbar />
+        <Slogan />
+    </div>
+  )
+}
+
+export default Home
